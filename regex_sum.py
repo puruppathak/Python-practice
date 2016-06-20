fe3372877cb2c032a4ea6886f776462c9819cac7
Puru Pathak <puruppathak@gmail.com>
2016-06-20 11:03:01 -0400

    Extracting numbers from a text file and calculating the sum

---
+++ regex_sum.py
@@ -0,0 +1,17 @@
+import re
+
+
+list_sum = 0           # Initializing sum variable
+hand = open ('trial.txt')      # Opening text file
+for line in hand:
+    line = line.rstrip()
+    y=re.findall('[0-9]+',line)  #Extracting digits from the file
+    if y:
+    	
+        for x in y:
+         list_sum += int(x)      # int(x) converts string to numbers
+
+if (list_sum == 0):
+	print "No numbers found in the text file"
+else:
+    print list_sum         
